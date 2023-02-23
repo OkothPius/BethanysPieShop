3d170033f39285102dbdc36c10258b6ed3f20aaf
Okoth Pius <orukopius8@gmail.com>
2023-02-23 14:34:22 +0300

    feat: update register order components to Database

--- BethanysPieShop/Data/AppDbContext.cs
+++ BethanysPieShop/Data/AppDbContext.cs
@@ -12,7 +12,8 @@ namespace BethanysPieShop.Data
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }  
-
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
