fdb4f41003fe9e08f91a3fc052702440ca7dea98
Okoth Pius <orukopius8@gmail.com>
2023-02-23 14:36:09 +0300

    feat: update MIgrations

---
+++ BethanysPieShop/Migrations/20230223073205_AddOrderModel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BethanysPieShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_shoppingCartItems_Pies_PieId",
+                table: "shoppingCartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_shoppingCartItems",
+                table: "shoppingCartItems");
+
+            migrationBuilder.RenameTable(
+                name: "shoppingCartItems",
+                newName: "ShoppingCartItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_shoppingCartItems_PieId",
+                table: "ShoppingCartItems",
+                newName: "IX_ShoppingCartItems_PieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShoppingCartItems",
+                table: "ShoppingCartItems",
+                column: "ShoppingCartItemId");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressLine1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressLine2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Zipcode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrderTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OrderPlaced = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    PieId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderDetailId);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Pies_PieId",
+                        column: x => x.PieId,
+                        principalTable: "Pies",
+                        principalColumn: "PieId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_PieId",
+                table: "OrderDetails",
+                column: "PieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItems_Pies_PieId",
+                table: "ShoppingCartItems",
+                column: "PieId",
+                principalTable: "Pies",
+                principalColumn: "PieId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItems_Pies_PieId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShoppingCartItems",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.RenameTable(
+                name: "ShoppingCartItems",
+                newName: "shoppingCartItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingCartItems_PieId",
+                table: "shoppingCartItems",
+                newName: "IX_shoppingCartItems_PieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_shoppingCartItems",
+                table: "shoppingCartItems",
+                column: "ShoppingCartItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_shoppingCartItems_Pies_PieId",
+                table: "shoppingCartItems",
+                column: "PieId",
+                principalTable: "Pies",
+                principalColumn: "PieId");
+        }
+    }
+}
